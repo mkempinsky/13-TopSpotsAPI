d4ad3dd1804d942dfe13c94d78798d78bbca302b
Michele Kempinsky <mkempinsky@icloud.com>
2016-08-28 17:33:38 -0700

    initial commit

---
+++ Server/TopSpotsAPI/TopSpotsAPI/App_Start/WebApiConfig.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace TopSpotsAPI
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            // tells the web API to convert normal case to pascal case
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+        }
+    }
+}
---
+++ Server/TopSpotsAPI/TopSpotsAPI/Controllers/TopSpotsController.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using TopSpotsAPI.Models;
+
+namespace TopSpotsAPI.Controllers
+{
+    public class TopSpotsController : ApiController
+    {
+        // GET: api/TopSpots
+        public IEnumerable<TopSpot> Get()
+        {
+            // get the file name
+            string fileName = HttpContext.Current.Server.MapPath("../App_Data/topSpots.json");
+
+            // read the context of the file into a string
+            string json = File.ReadAllText(fileName);
+
+            // convert the string into an array of objects of type TopSpot
+            IEnumerable<TopSpot> topSpots = JsonConvert.DeserializeObject<IEnumerable<TopSpot>>(json);
+
+            // return the array
+            return topSpots;
+        }
+
+        // GET: api/TopSpots/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/TopSpots
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/TopSpots/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/TopSpots/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
